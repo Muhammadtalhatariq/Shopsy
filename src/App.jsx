5c867c0cce8e8d573b4e5e06da89dad2d8171acc
muhammadtalha <mtalhatariqx@gmail.com>
2024-08-10 22:59:59 -0700

    assets done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
   return (
     <div className='bg-slate-400'>
-      shopsy
+     <Navbar/>
     </div>
   );
 }
